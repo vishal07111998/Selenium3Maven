a00f4d043cda1b9e8007edcdfe124837503f608a
vishal07111998 <vk267763@gmail.com>
2023-09-13 18:16:46 +0530

    added cucumber reports

--- src/test/java/UITestRunner.java
+++ src/test/java/UITestRunner.java
@@ -3,6 +3,10 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(strict = true,
         features = {"src/test/resources/feature/webUI"},
-        glue = "com.test.stepDefinitions")
+        glue = "com.test.stepDefinitions",
+        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "html:target/cucumber-reports"},
+        monochrome = true)
 public class UITestRunner extends AbstractTestNGCucumberTests {
 }
